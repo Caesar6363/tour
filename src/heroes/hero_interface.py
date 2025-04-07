ef55928d44858ff84b683c25f5e03c31dad0f799
SkripyKez <koruzkin@icloud.com>
2025-04-07 20:23:38 +0300

    refactor

--- src/heroes/hero_interface.py
+++ src/heroes/hero_interface.py
@@ -1,3 +1,5 @@
+import random as r
+from typing import Self
 from src.models.stats import HeroesStats
 class BaseHero:
 
@@ -17,6 +19,19 @@ class BaseHero:
         self.block_chance = stats.block_chance
         self.critical_damage_chance = stats.critical_damage_chance
 
+    def defense(self, mySelf, damage):
+        _damage = damage - (self.dodge_chance + self.block_chance)
+        print(f'{self.name}({self.hp}) блокирует {mySelf.name} и получает {_damage} урона')
+        print('')
+        self.hp = self.hp - _damage
+
+    def attack(self, enemy):
+        player2 = enemy
+        damage = r.choice(player2.damage) + r.choice(player2.critical_damage_chance)
+
+        print(f'{self.name}({self.hp}) бьет {player2.name} на {damage} урона')
+        player2.defense(self, damage)
+    
     def __repr__(self):
         return f"Имя: {self.name}, Ник: {self.nickname}, HP: {self.hp}, Оружие: {self.weapon_type}"
 
--- src/tournamnet.py
+++ src/tournamnet.py
@@ -11,25 +11,25 @@ class Fight:
     def __init__(self):
         self.rounds = 0
 
+    def getPlayers(self, heroes: list[BaseHero]) -> list[BaseHero]:
+        if len(heroes) <= 1:
+            return print("Нет участников для проведения турнира. Прорекламируйте ваш турнир:)")
 
+        player1 = r.choice(heroes)
+        heroes.remove(player1)
+        player2 = r.choice(heroes)
+        heroes.remove(player2)
 
-    def tournament(self, heroes: list[BaseHero]) -> tuple[BaseHero, BaseHero]:
+        return [player1, player2]
 
-        if len(heroes) > 1:
-            while len(heroes) > 1:
+    def tournament(self, heroes: list[BaseHero]):
+        
+        [player1, player2] = self.getPlayers(heroes)
 
-                player1 = r.choice(heroes)
-                heroes.remove(player1)
-                player2 = r.choice(heroes)
-                heroes.remove(player2)
-
-                self.rounds += 1
-                self.input_info(player1, player2)
-                win = self.duel(player1, player2)
-                heroes.append(win)
-                self.winner(heroes)
-        else:
-            print("Недостаточно участников для проведения турнира. Прорекламируйте ваш турнир:)")
+        self.rounds += 1
+        self.input_info(player1, player2)
+        win = self.duel(player1, player2)
+        self.winner(win)
 
 
     def duel(
@@ -37,87 +37,22 @@ class Fight:
             player1: Mage | Archer | Warrior,
             player2: Mage | Archer | Warrior
     ) -> Mage | Archer | Warrior:
-
+        
         while player1.hp > 0 and player2.hp > 0:
-
-            self.hit(player1, player2)
-            if player1.hp <= 0:
-                print(f"Игрок {player1.name} {player1.nickname} проигрывает бой и вылетает с турнира")
-                break
-
-            self.hit(player1, player2)
-
-            if player2.hp <= 0:
-                print(f"Игрок {player2.name} {player2.nickname} проигрывает бой и вылетает с турнира")
-                break
-            print()
-            self.print_hp(player1, player2)
+            if player1.hp > 0:
+                player1.attack(player2)
+            if player2.hp > 0:
+                player2.attack(player1)
 
         if player1.hp > 0:
-            player1.hp = 100
             return player1
-
-        player2.hp = 100
-        return player2
-
-    @staticmethod
-    def hit(
-            player1: Mage | Archer | Warrior,
-            player2: Mage | Archer | Warrior
-    ):
-        r_damage_p1 = r.choice(player1.damage)
-        r_crit_damage_p1 = r.choice(player1.critical_damage_chance)
-        sum_damage_crit1 = r_damage_p1 + r_crit_damage_p1
-        sum_dodge_block1 = player2.dodge_chance + player2.block_chance
-        player1_damage = sum_damage_crit1 - sum_dodge_block1
-        player1_damage = max(player1_damage, 0)
-
-        player2.hp -= player1_damage
-        # self.print_hit(player1, player2)
-        print()
-        print(f'Игрок {player1.name} {player1.nickname} нанес удар, '
-              f'Нанес урона: {r_damage_p1}, крит {r_crit_damage_p1} Оружием: {player1.weapon_type},'
-              f' Игроку: {player2.name}')
-        print(f'Игрок {player2.name} {player2.nickname} '
-              f'получил с удара {sum_damage_crit1} урона, '
-              f'но смог заблокировать {sum_dodge_block1} урона. '
-              f'Полученный урон {player1_damage}')
-
-        r_damage_p2 = r.choice(player2.damage)
-        r_crit_damage_p2 = r.choice(player2.critical_damage_chance)
-        sum_damage_crit2 = r_damage_p2 + r_crit_damage_p2
-        sum_dodge_block2 = player1.dodge_chance + player1.block_chance
-        player2_damage = sum_damage_crit2 - sum_dodge_block2
-        player2_damage = max(player2_damage, 0)
-        player1.hp -= player2_damage
-        # self.print_hit(player2, player1)
-        print()
-        print(f'Игрок {player2.name} {player2.nickname} нанес удар, '
-              f'Нанес урона: {r_damage_p2}, крит {r_crit_damage_p2} Оружием: {player1.weapon_type},'
-              f' Игроку: {player1.name}')
-        print(f'Игрок {player1.name} {player1.nickname} '
-              f'получил с удара {sum_damage_crit2} урона, '
-              f'но смог заблокировать {sum_dodge_block2} урона. '
-              f'Полученный урон {player2_damage}')
-
+        else:
+            return player2
 
 
     def input_info(self, player1, player2):
-        print()
         print(f"Раунд {self.rounds}\n\nИмя первого игрока: {player1.name}, Никнейм первого игрока: {player1.nickname}\n"
-              f"Имя второго игрока: {player2.name}, Никнейм второго игрока: {player2.nickname}")
-
-
-    #
-    # @staticmethod
-    # def print_hit(player1, player2):
-
-        # time.sleep(2)
-        # damage = r.choice(player1.damage)
-        # crit = r.choice(player1.critical_damage_chance)
-        # print(f'Игрок {player1.name} {player1.nickname} нанес удар, '
-        #       f'Нанес урона: {damage}, крит {crit} Оружием: {player1.weapon_type},'
-        #       f' Игроку: {player2.name}')
+              f"Имя второго игрока: {player2.name}, Никнейм второго игрока: {player2.nickname}\n")
 
 
     @staticmethod
@@ -129,8 +64,4 @@ class Fight:
 
     @staticmethod
     def winner(player):
-
-        if len(player) <= 1:
-            # time.sleep(2)
-            print()
-            print(f"Победитель турнира: {player[0].name} {player[0].nickname}")
+        print(f"Победитель турнира: {player.name} {player.nickname}")
