1378c80fd9cf9d32e4605e28e84f95fd12d7d1cf
SkripyKez <koruzkin@icloud.com>
2025-04-14 14:45:03 +0300

    test

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
-from flask import request
-
+from flask import (Flask, request)
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return '<Player %r>' % self.id
+
+# Создание базы данных внутри контекста приложения
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def hello():
@@ -11,7 +24,17 @@ def hello():
 @app.route('/user', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        print(request)
+        try:
+            db.session.add(Player(name='dima'))
+            db.session.commit()
+        except:
+            print('error')
         return 'POST'
     else:
+        try:
+            res = Player.query.order_by(Player.name).all()
+            
+            print(res[2].name)
+        except:
+            print('error')
         return 'GET'
